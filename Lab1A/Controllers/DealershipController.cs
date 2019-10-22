9e689c00a54edf0878bbc7c9bfab758832b11b17
Khan <000361706@mohawkcollege.ca>
2019-10-22 16:11:32 -0400

    Add project files.

---
+++ Lab1A/Controllers/DealershipController.cs
@@ -0,0 +1,176 @@
+﻿//I, Bilal Khan, student number 000361706, certify that this material is my
+//original work.No other person's work has been used without due
+//acknowledgement and I have not made my work available to anyone else.
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Lab1A.Models;
+
+namespace Lab1A.Controllers
+{
+    public static class DealershipMgr
+    {
+        public static List<Dealership> dealerships()
+        {
+            List<Dealership> dealerships = new List<Dealership>();
+            dealerships.Add(new Dealership { Name = "Dealer1", Email = "dealer@1.com", PhoneNumber = "647-123-4567" });
+            dealerships.Add(new Dealership { Name = "Dealer2", Email = "dealer@2.com", PhoneNumber = "905-123-4567" });
+            dealerships.Add(new Dealership { Name = "Dealer3", Email = "dealer@3.com", PhoneNumber = "416-123-4567" });
+            return dealerships;
+        }
+    }
+    public class DealershipController : Controller
+    {
+        private readonly Lab1AContext _context;
+        private readonly List<Dealership> _dealerships;
+
+        public DealershipController(Lab1AContext context)
+        {
+            _context = context;
+            _dealerships = DealershipMgr.dealerships();
+        }
+
+        // GET: Dealership
+        public async Task<IActionResult> Index()
+        {
+            return View(_dealerships.ToArray());
+            //return View(await _context.Dealership.ToListAsync());
+        }
+
+        // GET: Dealership/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            //var dealership = await _context.Dealership
+            //    .FirstOrDefaultAsync(m => m.ID == id);
+            //if (dealership == null)
+            //{
+            //    return NotFound();
+            //}
+            var dealership = _dealerships.ToArray()
+                .FirstOrDefault(m => m.ID == id);
+            if (dealership == null)
+            {
+                return NotFound();
+            }
+
+            return View(dealership);
+        }
+
+        // GET: Dealership/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Dealership/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ID,Name,Email,PhoneNumber")] Dealership dealership)
+        {
+            if (ModelState.IsValid)
+            {
+                _dealerships.Add(dealership);
+                //_context.Add(dealership);
+                //await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(dealership);
+        }
+
+        // GET: Dealership/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var dealership = await _context.Dealership.FindAsync(id);
+            if (dealership == null)
+            {
+                return NotFound();
+            }
+            return View(dealership);
+        }
+
+        // POST: Dealership/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ID,Name,Email,PhoneNumber")] Dealership dealership)
+        {
+            if (id != dealership.ID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(dealership);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!DealershipExists(dealership.ID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(dealership);
+        }
+
+        // GET: Dealership/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var dealership = await _context.Dealership
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (dealership == null)
+            {
+                return NotFound();
+            }
+
+            return View(dealership);
+        }
+
+        // POST: Dealership/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var dealership = await _context.Dealership.FindAsync(id);
+            _context.Dealership.Remove(dealership);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool DealershipExists(int id)
+        {
+            return _context.Dealership.Any(e => e.ID == id);
+        }
+    }
+}
